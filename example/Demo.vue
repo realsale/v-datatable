bb0e045fb02eebec3d30fd93bec09652a2588237
realsale <pwedesaken@gmail.com>
2022-01-31 22:00:07 +0800

    modify src dir and add example dir

--- example/Demo.vue
+++ example/Demo.vue
@@ -1,18 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <DataTable />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import DataTable from "../src/components/DataTable.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    DataTable,
   },
+  data() {
+    return {
+
+    }
+  }
 };
 </script>
 
